77f722100838262023d8461d144ac2ecc8a5e305
Диана Соколова <dosokolova@edu.hse.ru>
2021-07-23 15:54:49 +0500

    1

--- google_sheets.py
+++ google_sheets.py
@@ -15,8 +15,8 @@ gc = gspread.service_account(filename=CREDENTIALS_FILE)
 
 sht = gc.open_by_url('https://docs.google.com/spreadsheets/d/1Yx3HqvBdnMn4Uc70rjH3HKFgqZXDFkoFIqLRfRwciZ0')
 
-
 # sht.add_worksheet("Test", rows='100', cols='5')
+sht.worksheet('Test').append_row([1, 1, 1, 1, 1])
 
 
 def find_sheet(name):
@@ -28,4 +28,4 @@ def find_sheet(name):
 
 
 def write_data(name, hotel, room_number, date, number_of_people, sheet):
-    sheet.append_row()
+    sheet.append_row([name, hotel, room_number, date, number_of_people])
